44bdbb4934b877387e12f92d80be059e6952b5f8
Joseph <jchen1@exeter.edu>
2020-06-07 14:03:14 -0700

    Add dropdown

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a href="#">
+      {{ status }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dropdown',
+  props: ['status', 'statuses']
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/components/EssayList.vue
+++ src/components/EssayList.vue
@@ -1,31 +1,48 @@
 <template>
   <div>
-      <input type="text" class="essay-input" placeholder="What needs to be done"
-      v-model="newEssay" @keyup.enter="addEssay">
-      <div v-for="(essay, index) in essays" :key="essay.id" class="essay-item">
-          <div class="essay-item-left">
-              <input type="checkbox" v-model="essay.completed">
-              <div v-if="!essay.editing" @dblclick="editEssay(essay)" class="essay-item-label">
-                  {{essay.title }}
-              </div>
-              <input v-else class="essay-item-edit" type="text"
-              v-model="essay.title" @blur="doneEdit(essay)" @keyup.enter="doneEdit(essay)" v-focus>
-          </div>
-          <div class="remove-item" @click="removeEssay(index)">
-              &times;
-          </div>
+    <input
+      type="text"
+      class="essay-input"
+      placeholder="What needs to be done"
+      v-model="newEssay"
+      @keyup.enter="addEssay"
+    />
+    <div v-for="(essay, index) in essays" :key="essay.id" class="essay-item">
+      <div class="essay-item-left">
+        <div>
+          <Dropdown status="Ready to Start" :items="statuses" />
+        </div>
+        <input type="checkbox" v-model="essay.completed" />
+        <div
+          v-if="!essay.editing"
+          @dblclick="editEssay(essay)"
+          class="essay-item-label"
+        >{{essay.title }}</div>
+        <input
+          v-else
+          class="essay-item-edit"
+          type="text"
+          v-model="essay.title"
+          @blur="doneEdit(essay)"
+          @keyup.enter="doneEdit(essay)"
+          v-focus
+        />
       </div>
-      <div>
-          <div class="count-left">
-              {{ remaining }} items left
-          </div>
-      </div>
+      <div class="remove-item" @click="removeEssay(index)">&times;</div>
+    </div>
+    <div>
+      <div class="count-left">{{ remaining }} items left</div>
+    </div>
   </div>
 </template>
 
 <script>
+import Dropdown from './Dropdown.vue'
 export default {
   name: 'essay-list',
+  components: {
+    Dropdown
+  },
   data () {
     return {
       newEssay: '',
@@ -91,61 +108,61 @@ export default {
 </script>
 
 <style lang="scss">
-    #checkbox {
-        padding: 50px;
-    }
+#checkbox {
+  padding: 50px;
+}
 
-    .essay-input {
-        width: 100%;
-        padding: 10px 18px;
-        font-size: 18px;
-        margin-bottom: 16px;
+.essay-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
 
-        &:focus {
-            outline: 0;
-        }
-    }
+  &:focus {
+    outline: 0;
+  }
+}
 
-    .essay-item {
-        margin-bottom: 12px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
+.essay-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
-    .count-left {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        font-size: 16px;
-        border-top: 1px solid lightgrey;
-        padding-top: 14px;
-        margin-bottom: 14px;
-    }
+.count-left {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
 
-    .remove-item {
-        cursor: pointer;
-        margin-left: 14px;
-        &:hover {
-            color: black;
-        }
-    }
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
+  }
+}
 
-    .essay-item-left {
-        display: flex;
-        align-items: center;
-    }
-    .essay-item-edit {
-        font-size: 20px;
-        color: #2c3e50;
-        margin-left: 12px;
-        width: 100%;
-        padding: 10px;
-        border: 1px solid #ccc;
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+.essay-item-left {
+  display: flex;
+  align-items: center;
+}
+.essay-item-edit {
+  font-size: 20px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 
-        &:focus {
-            outline: none;
-        }
-    }
+  &:focus {
+    outline: none;
+  }
+}
 </style>
