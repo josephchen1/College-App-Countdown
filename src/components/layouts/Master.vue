e0f0cdf10a1ba60d194f7cf6432ceb9220ec43a9
Joseph <jchen1@exeter.edu>
2020-07-16 10:00:07 -0700

    Add asset photos, add new pages, add files into .gitignore, and add footer with icons

--- src/components/layouts/Master.vue
+++ src/components/layouts/Master.vue
@@ -1,19 +1,52 @@
 <template>
     <div id="app">
-<b-navbar>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <b-navbar>
         <template slot="brand">
             <b-navbar-item tag="router-link" :to="{ name: 'home' }">
-<h2 class="title is-4">ESSAYTRACK</h2>
+      <h2 class="title is-4">ESSAYTRACK</h2>
             </b-navbar-item>
         </template>
         <template slot="start">
             <b-navbar-item tag="router-link" :to="{ name: 'home' }">Home</b-navbar-item>
             <b-navbar-item tag="router-link" :to="{ name: 'essay' }">App</b-navbar-item>
-                <b-navbar-item tag="router-link" :to="{ name: 'about' }">About</b-navbar-item>
+            <!-- <b-navbar-item tag="router-link" :to="{ name: 'about' }">About</b-navbar-item> -->
                             <top-header></top-header>
         </template>
     </b-navbar>
         <router-view></router-view>
+        <footer class="footer-distributed">
+    <div class="footer-right">
+      <a href="https://www.facebook.com/sephhiiee/" target="_blank">
+        <i class="fa fa-facebook"></i>
+      </a>
+      <a href="https://twitter.com/sephhiiee" target="_blank">
+        <i class="fa fa-twitter"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/josephchen214/" target="_blank">
+        <i class="fa fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/josephchen1/" target="_blank">
+        <i class="fa fa-github"></i>
+      </a>
+    </div>
+
+    <div class="footer-left">
+      <p class="footer-links">
+        <a @click="$router.push('/')">Home</a>
+        ·
+        <a @click="$router.push('about')">About</a>
+        ·
+        <a @click="$router.push('faq')">FAQ</a>
+         ·
+        <a @click="$router.push('privacy')">Privacy</a>
+        ·
+        <a @click="$router.push('contact')">Contact</a>
+      </p>
+
+      <p>Joseph Chen &copy; 2020</p>
+    </div>
+  </footer>
     </div>
 </template>
 
@@ -57,14 +90,87 @@ export default {
   }
   .navbar-end{
   display: none;
-}
-.navbar-start{
-  width: 100%;
-  -webkit-box-pack: end !important;
-    -ms-flex-pack: end !important;
-    justify-content: flex-end !important;
-}
-.container{
-  padding: 0 15px;
-}
+  }
+  .navbar-start{
+    width: 100%;
+    -webkit-box-pack: end !important;
+      -ms-flex-pack: end !important;
+      justify-content: flex-end !important;
+  }
+  .container{
+    padding: 0 15px;
+  }
+  .footer-distributed {
+    background-color: #c4aeeb;
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+    box-sizing: border-box;
+    width: 100%;
+    text-align: left;
+    font: normal 16px sans-serif;
+
+    padding: 45px 50px;
+    margin-top: 0px;
+  }
+
+  .footer-distributed .footer-left p {
+    color: #ffffff;
+    font-size: 14px;
+    margin: 0;
+  }
+
+  /* Footer links */
+
+  .footer-distributed p.footer-links {
+    font-size: 18px;
+    font-weight: bold;
+    color: #ffffff;
+    margin: 0 0 10px;
+    padding: 0;
+  }
+
+  .footer-distributed p.footer-links a {
+    display: inline-block;
+    line-height: 1.8;
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .footer-distributed .footer-right {
+    float: right;
+    margin-top: 6px;
+    max-width: 180px;
+  }
+
+  .footer-distributed .footer-right a {
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    background-color: #714fa9;
+    border-radius: 2px;
+
+    font-size: 20px;
+    color: #ffffff;
+    text-align: center;
+    line-height: 35px;
+
+    margin-left: 3px;
+  }
+
+  /* If you don't want the footer to be responsive, remove these media queries */
+
+  @media (max-width: 600px) {
+    .footer-distributed .footer-left,
+    .footer-distributed .footer-right {
+      text-align: center;
+    }
+
+    .footer-distributed .footer-right {
+      float: none;
+      margin: 0 auto 20px;
+    }
+
+    .footer-distributed .footer-left p.footer-links {
+      line-height: 1.8;
+    }
+  }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import App from './App.vue'
 import LandingPage from './components/marketing/LandingPage'
 import AboutPage from './components/marketing/AboutPage'
+import FAQ from './components/marketing/FAQ'
+import Privacy from './components/marketing/Privacy'
+import Contact from './components/marketing/Contact'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
@@ -9,7 +12,10 @@ const routes = [
   { path: '/essay', name: 'essay', component: App, meta: { requiresAuth: true } },
   { path: '/about', name: 'about', component: AboutPage },
   { path: '/login', name: 'login', component: Login },
-  { path: '/register', name: 'register', component: Register }
+  { path: '/register', name: 'register', component: Register },
+  { path: '/faq', name: 'faq', component: FAQ },
+  { path: '/contact', name: 'contact', component: Contact },
+  { path: '/privacy', name: 'privacy', component: Privacy }
 ]
 
 export default routes
